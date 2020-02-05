1e9dc348866b01174b242485e6b6d30482276f44
8luebottle <itbiz.irs@gmail.com>
2020-02-05 13:16:57 +0900

    Parsing JSON
    addCats()

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
-
+	"log"
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 )
+
+type Cat struct{
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
 
 func begin(c echo.Context) error {
 	return c.String(http.StatusOK, "SUCCESS!")
@@ -32,6 +39,26 @@ func getCats(c echo.Context) error {
 	}) // Neither of those condition's match than return this
 }
 
+func addCats(c echo.Context) error{
+	cat := Cat{}
+
+	defer c.Request().Body.Close()
+	b, err := ioutil.ReadAll(c.Request().Body) // b : body
+	if err != nil{
+		log.Printf("Failed reading the request body : %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	err = json.Unmarshal(b, &cat) // Memory address of cat
+	if err != nil {
+		log.Printf("Failed unmarshaling in addCats: %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	log.Printf("this is your cat: %#v", cat)
+	return c.String(http.StatusOK, "We got your cat!")
+}
+
 func main() {
 	fmt.Println("It's a good beginning for me.")
 
@@ -39,6 +66,8 @@ func main() {
 
 	e.GET("/", begin) 		 // Endpoint1
 	e.GET("/cats/:data", getCats) // Endpoint2
+
+	e.POST("/cats", addCats)
 
 	// Routes
 	e.Start(":8000") // Start Server
